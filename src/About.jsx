73c04aaac6f37c51f157cf42a0db6b01548157e3
nitnkmr <nitin84498@gmail.com>
2023-02-15 18:51:53 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='about'>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis reiciendis repellat error omnis esse. Officiis veritatis voluptas provident labore, odit corporis. Numquam repellat laboriosam magni sint laudantium facere optio minus.</p>
+        <p>Lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint nam obcaecati, perspiciatis rem voluptatem quis a nostrum deleniti doloribus. Cumque, aliquid voluptatem. Similique ex fugit obcaecati, cupiditate et quas tempore. ipsum dolor sit amet consectetur adipisicing elit. Blanditiis reiciendis repellat error omnis esse. Officiis veritatis voluptas provident labore, odit corporis. Numquam repellat laboriosam magni sint laudantium facere optio minus.</p>
+        <p>Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore repellendus, exercitationem laborum voluptates eveniet rerum officiis adipisci id fugiat possimus obcaecati quis reprehenderit impedit cupiditate maiores consequuntur dolorum cumque porro.------- dolor sit amet consectetur adipisicing elit. Blanditiis reiciendis repellat error omnis esse. Officiis veritatis voluptas provident labore, odit corporis. Numquam repellat laboriosam magni sint laudantium facere optio minus.</p>
+    </div>
+  )
+}
